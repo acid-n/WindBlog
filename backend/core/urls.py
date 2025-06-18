5b961f6c26ce9921705789e0f44433b726451c37
Nikolay Shapovalov <critical.nick@gmail.com>
2025-06-18 20:31:46 +0300

    feat: перенесен роутинг SiteSettings в core

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,6 @@
-from core.api import SiteSettingsViewSet
 from rest_framework.routers import DefaultRouter
+
+from .api import SiteSettingsViewSet
 
 router = DefaultRouter()
 router.register("site-settings", SiteSettingsViewSet, basename="site-settings")
